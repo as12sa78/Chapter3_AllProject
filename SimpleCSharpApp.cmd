64bcf214c662372c4c24984faa70ccdebc38a9f2
Vit Mar <as12sa78@gmail.com>
2024-11-06 14:08:31 +0300

    Add project files.

---
+++ SimpleCSharpApp.cmd
@@ -0,0 +1,20 @@
+@echo off
+rem Пакетный файл для приложения SimpleCSharpApp.exe,
+rem в котором захватывается возвращаемое значение.
+
+dotnet run
+@if "%ERRORLEVEL%" == "0" goto succes
+
+:fail
+  rem Приложение потерпело неудачу.
+  echo This application has failed!
+  echo return value = %ERRORLEVEL%
+  goto end
+:success
+  rem Приложение завершилось успешно.
+  echo This application has succeeded!
+  echo return value = value = %ERRORLEVEL%
+  goto end
+:end
+rem Всё готово.
+rem All Done.
---
+++ SimpleCSharpApp/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+// Обратие внимание, что теперь возвращается int, а не void.
+// Вывести сообщение и ожидать нажатия клавиши <Enter>.
+Console.WriteLine("***** My First C# App *****");
+Console.WriteLine("Hello World!");
+Console.WriteLine();
+Console.ReadLine();
+
+// Возвратить произвольный код ошибки.
+//return -1;
