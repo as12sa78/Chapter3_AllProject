9aac85c71b97a3d84c302233851b6f8fd9ffacab
Vit Mar <as12sa78@gmail.com>
2024-11-06 14:15:01 +0300

    обработка массива аргументов в main

--- SimpleCSharpApp/Program.cs
+++ SimpleCSharpApp/Program.cs
@@ -5,7 +5,14 @@
 Console.WriteLine("***** My First C# App *****");
 Console.WriteLine("Hello World!");
 Console.WriteLine();
+// Обработать любые входные аргументы.
+for (int i = 0; i < args.Length; i++)
+{
+    Console.WriteLine("Arg: {0}", args[i]);
+}
+
 Console.ReadLine();
 
+
 // Возвратить произвольный код ошибки.
-//return -1;
+//return 0;
